b399775577b94552aa608977c5174dfe9fb03b64
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-03-16 10:33:48 -0400

    fixed base html and css

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var timer = 0;
+var questions = [
+    {q:"Question 1", 
+     a: ["ans1", "ans2", "ans3", "ans4"],
+     correctAns: "ans2"},
+     {q:"Question 2", 
+     a: ["ans1", "ans2", "ans3", "ans4"],
+     correctAns: "ans3"},
+     {q:"Question 3", 
+     a: ["ans1", "ans2", "ans3", "ans4"],
+     correctAns: "ans1"},
+     {q:"Question 4", 
+     a: ["ans1", "ans2", "ans3", "ans4"],
+     correctAns: "ans4"},
+     {q:"Question 5", 
+     a: ["ans1", "ans2", "ans3", "ans4"],
+     correctAns: "ans1"}
+];
